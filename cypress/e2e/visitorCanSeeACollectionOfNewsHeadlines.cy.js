4f957a587a8d2002d2711bf2e4297952077a2b38
shagufta <shagufta.batool8@gmail.com>
2022-06-11 01:30:41 +0200

    passes first get request test

--- cypress/e2e/visitorCanSeeACollectionOfNewsHeadlines.cy.js
+++ cypress/e2e/visitorCanSeeACollectionOfNewsHeadlines.cy.js
@@ -1,15 +1,19 @@
 describe("user can see collection of top news headlines ", () => {
-
-   beforeEach(() => {
-     cy.intercept("GET", "https://newsapi.org/v2/top-headlines**", {
-       fixture: "news_index.json",
-     }).as("getNewsFeed");
-     cy.visit("/");
-   });
-
-  it("is expected to make a GET request to the API", () => {
-    
-    cy.wait("@getNewsFeed").its("request.method").should("eq", "GET");
+  beforeEach(() => {
+    cy.intercept("GET", "https://newsapi.org/v2/top-headlines**", {
+      fixture: "news_index.json",
+    }).as("getNewsFeed");
+    cy.visit("/");
   });
 
+  it("is expected to make a GET request to the API", () => {
+    cy.wait("@getNewsFeed").its("request.method").should("eq", "GET");
+  });
+  it("is expected to display a collection of 20 articles", () => {
+    cy.get("[data-cy=articles-list]")
+      .children()
+      .first()
+      .children()
+      .should("have.length", 20);
+  });
 });
