e5dea3d81b63c44151d9d4194f649f31a3524ec4
miyeaier <miyesier@gmail.com>
2022-06-10 12:09:22 +0200

    wirte frist test to begin project yarn add axios yarn add redux

--- cypress/e2e/visitorCanSeeACollectionOfNewsItems.cy.js
+++ cypress/e2e/visitorCanSeeACollectionOfNewsItems.cy.js
@@ -1,5 +1,11 @@
-describe("", () => {
-  it("", () => {
+describe("Display news headlines", () => {
+  before(() => {
+    cy.intercept("GET", "https://newsapi.org/v2/top-headlines**", {
+      fixture: "news_index.json",
+    });
     cy.visit("/");
   });
-});
+  it("is expected to see a header", () => {
+    cy.get("[data-cy=news-header]").should("contain", "News Wire Network");
+  });
+})
