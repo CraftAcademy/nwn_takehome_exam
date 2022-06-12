1d74ba57c2b417c64696a24de2bbf7cb67122044
miyeaier <miyesier@gmail.com>
2022-06-12 13:06:43 +0200

    changes the method to store and access data on srticles

--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { useSelector} from 'react-redux'
-
-
+import React from "react";
+import NewsCard from "./NewsCard";
+import { useSelector, useDispatch } from "react-redux";
 
 const NewsIndex = () => {
- 
+  const dispatch = useDispatch();
+  const articles = useSelector((state) => state.newsFeed);
+
+  useEffect(() => {
+    NewsSerevice.index(dispatch);
+  }, []);
+
+  let listOfTopHeadlines = articles.map(() => {
+    return <NewsCard article={article} />;
+  });
 
   return (
     <>
-
+      <Grid data-cy="articles">{listOfTopHeadlines}</Grid>
     </>
-  )
-}
+  );
+};
 
-export default NewsIndex
+export default NewsIndex;
