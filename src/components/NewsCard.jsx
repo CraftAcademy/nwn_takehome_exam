ab70c2480d24aff4d32a5ba9e0b52af4903e1bab
shagufta <shagufta.batool8@gmail.com>
2022-06-12 14:18:22 +0200

    refactor NewsCard component

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -4,14 +4,13 @@ import { Header, Card } from 'semantic-ui-react'
 const NewsCard = (props) => {
   return (
     <Card
-      image='https://editorial.fxstreet.com/images/Markets/Currencies/Digital%20Currencies/Bitcoin/bitcoins-52602600_Large.jpg'
-      header='DBS Bank to launch cryptocurrency exchange after condemning Bitcoin a Ponzi scheme in 2017'
-      meta={`By: ${'Lorenzo Stroe'} at ${'FXStreet'}`}
-      description='DBS Bank of Singapore has just announced the launch of a digital asset exchange featuring Bitcoin, Ethereum, Bitcoin Cash, and XRP and several fiat cu'
-      extra={<Header as='h6'>`Published at ${"2020-12-10T20:29:20Z"}`</Header>}
-
+      image={props.article.urlToImage}
+      header={props.article.title}
+      meta={`By: ${props.article.author} at ${props.article.source.name}`}
+      description={props.article.description}
+      extra={<Header as="h6">`Published at ${props.news.publishedAt}`</Header>}
     />
-  )
+  );
 }
 
 export default NewsCard
