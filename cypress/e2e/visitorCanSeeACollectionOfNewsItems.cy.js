84fc90ed86f376914de6e1995c327fdad0dcc452
miyeaier <miyesier@gmail.com>
2022-06-12 23:27:59 +0200

    add test for user can see list of news format code and delete unuseless import

--- cypress/e2e/visitorCanSeeACollectionOfNewsItems.cy.js
+++ cypress/e2e/visitorCanSeeACollectionOfNewsItems.cy.js
@@ -5,7 +5,23 @@ describe("Display news headlines", () => {
     });
     cy.visit("/");
   });
+
   it("is expected to see 20 list items", () => {
     cy.get("[data-cy=articles]").children().should("have.length", 20);
+  });
+
+  describe("user can see list of news", () => {
+    it("is expected to see first article", () => {
+      cy.get("[data-cy=articles]")
+        .first()
+        .within(() => {
+          cy.get(".image").should("exist");
+          cy.get(".header").should(
+            "contain",
+            "How Do Bitcoin Transactions Work?"
+          );
+          cy.get(".description").should("exist");
+        });
+    });
   });
 })
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Header, Card } from "semantic-ui-react";
 
-const NewsCard = ({article}) => {
-  
+const NewsCard = ({ article }) => {
   return (
     <Card
-    data-cy={`article-${article.id}`}
-    key={article.index}
-    image={article.urlToImage}
-    header={article.title}
-    meta={`By: ${article.author} at ${article.name}`}
-    description={article.description}
-    extra={<Header as="h6">{`Published at ${article.publishedAt}`}</Header>}
-    href={article.url}
-  />
+      data-cy={`article-${article.id}`}
+      key={article.index}
+      image={article.urlToImage}
+      header={article.title}
+      meta={`By: ${article.author} at ${article.name}`}
+      description={article.description}
+      extra={<Header as="h6">{`Published at ${article.publishedAt}`}</Header>}
+      href={article.url}
+    />
   );
 };
 
