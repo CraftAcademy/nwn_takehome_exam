bac54070a96f4d4c2d07305be88699ed1fd4216e
miyeaier <miyesier@gmail.com>
2022-06-13 08:34:25 +0200

    remove debugger format code

--- src/components/NewsIndex.jsx
+++ src/components/NewsIndex.jsx
@@ -19,9 +19,7 @@ const NewsIndex = () => {
     dispatch({ type: "SET_NEWS_FEED", payload: response.data.articles });
   };
   const listOfTopHeadlines = articles.map((article) => {
-    debugger
     return <NewsCard article={article} />;
-   
   });
   return (
     <>
